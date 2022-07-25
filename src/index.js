03247a89f10bad3df7829934494daf5a3811d765
jenesh <jenesh@dzangolab.com>
2022-07-25 22:10:23 +0545

    Feat(Fastify) - create a server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, reply) => {
+  reply.send({ hello: "world" });
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 5000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
